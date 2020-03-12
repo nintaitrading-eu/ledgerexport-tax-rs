d765687c7c5f76e1b91715cbeaabf9c695d1eb43
Andy Nagels <thereisanewway@gmail.com>
2020-03-12 10:14:31 +0100

    Moved tests to seperate file.

--- src/main.rs
+++ src/main.rs
@@ -190,16 +190,4 @@ fn write_lines_to_pdf(alayer: &PdfLayerReference, afont: &IndirectFontRef, aline
 }
 
 #[cfg(test)]
-mod tests
-{
-    use super::*;
-
-    #[test]
-    fn test_daterange_from_quarter()
-    {
-        assert_eq!(get_daterange_from_quarter(1, 2019, true), "2019-01-01");
-        assert_eq!(get_daterange_from_quarter(1, 2019, false), "2019-04-01");
-        assert_eq!(get_daterange_from_quarter(4, 2020, true), "2020-10-01");
-        assert_eq!(get_daterange_from_quarter(4, 2020, false), "2021-01-01");
-    }
-}
+mod main_test;
---
+++ src/main_test.rs
@@ -0,0 +1,10 @@
+use super::*;
+
+#[test]
+fn test_daterange_from_quarter()
+{
+    assert_eq!(get_daterange_from_quarter(1, 2019, true), "2019-01-01");
+    assert_eq!(get_daterange_from_quarter(1, 2019, false), "2019-04-01");
+    assert_eq!(get_daterange_from_quarter(4, 2020, true), "2020-10-01");
+    assert_eq!(get_daterange_from_quarter(4, 2020, false), "2021-01-01");
+}
